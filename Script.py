f6abb5bca31e66e31624cb4fb7f621c2294a461d
sukhwindersingh36330-sys <sukhwindersingh36330@gmail.com>
2026-05-29 17:33:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -291,16 +291,16 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
-</b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
+<b><a href={poster_url}>📥</a><a href={imdb_url}>New {tag} Added</a></b>
 
-<blockquote>✨ ᴛɪᴛʟᴇ : <code>{filename} {year}</code>
+<b>✨ ᴛɪᴛʟᴇ : <code>{filename} {year}</code>
 
 🎭 ɢᴇɴʀᴇs : <b>{genres}</b>
 📺 ᴏᴛᴛ        : <b>{ott}</b>
 🎞️ ǫᴜᴀʟɪᴛʏ : <b>{quality}</b>
 🎧 ᴀᴜᴅɪᴏ    : <b>{language}</b>
 🔥 ʀᴀᴛɪɴɢ   : <b>{rating}</b>
-</blockquote>
+</b>
 
 🔍 <b>Sᴇᴀʀᴄʜ →</b> {search_link}
 """
